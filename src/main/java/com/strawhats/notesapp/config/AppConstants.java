99632fde58a6af5ccd405454ceed4dee0f1d4afd
lrdluffy <lrdaftkhar@gmail.com>
2025-07-08 15:40:58 +0330

    Controller layer implemented

---
+++ src/main/java/com/strawhats/notesapp/config/AppConstants.java
@@ -0,0 +1,9 @@
+package com.strawhats.notesapp.config;
+
+public class AppConstants {
+
+    public static final String PAGE_NUMBER = "10";
+    public static final String PAGE_SIZE = "10";
+    public static final String SORT_NOTE_BY = "id";
+    public static final String SORT_ORDER = "ASC";
+}
---
+++ src/main/java/com/strawhats/notesapp/controller/NoteController.java
@@ -0,0 +1,68 @@
+package com.strawhats.notesapp.controller;
+
+import com.strawhats.notesapp.config.AppConstants;
+import com.strawhats.notesapp.payload.NoteDTO;
+import com.strawhats.notesapp.payload.NoteResponse;
+import com.strawhats.notesapp.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class NoteController {
+
+    private final NoteService noteService;
+
+    @Autowired
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @PostMapping("/note")
+    public ResponseEntity<NoteDTO> createNote(@RequestBody NoteDTO noteDTO) {
+        NoteDTO savedNoteDTO = noteService.createNote(noteDTO);
+        return new ResponseEntity<>(savedNoteDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/notes")
+    public ResponseEntity<NoteResponse> getAllNotes(
+            @RequestParam(defaultValue = AppConstants.PAGE_NUMBER) Integer pageNumber,
+            @RequestParam(defaultValue = AppConstants.PAGE_SIZE) Integer pageSize,
+            @RequestParam(defaultValue = AppConstants.SORT_NOTE_BY) String sortBy,
+            @RequestParam(defaultValue = AppConstants.SORT_ORDER) String sortOrder
+    ) {
+        NoteResponse noteResponse = noteService.getAllNotes(pageNumber, pageSize, sortBy, sortOrder);
+        return new ResponseEntity<>(noteResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/notes/{noteId}")
+    public ResponseEntity<NoteDTO> getNoteById(
+            @PathVariable Long noteId
+    ){
+        NoteDTO noteDTO = noteService.getNoteById(noteId);
+        return new ResponseEntity<>(noteDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/notes/{noteId}")
+    public ResponseEntity<NoteDTO> updateNote(
+            @PathVariable Long noteId,
+            @RequestBody NoteDTO noteDTO
+    ) {
+        NoteDTO updatedNoteDTO = noteService.updateNote(noteId, noteDTO);
+        return new ResponseEntity<>(updatedNoteDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/notes/{noteId}")
+    public ResponseEntity<NoteDTO> deleteNote(@PathVariable Long noteId) {
+        NoteDTO deletedNoteDTO = noteService.deleteNote(noteId);
+        return new ResponseEntity<>(deletedNoteDTO, HttpStatus.OK);
+    }
+
+    @PatchMapping("/notes/{noteId}/archive")
+    public ResponseEntity<NoteDTO> archiveNote(@PathVariable Long noteId) {
+        NoteDTO archivedNoteDTO = noteService.archiveNote(noteId);
+        return new ResponseEntity<>(archivedNoteDTO, HttpStatus.OK);
+    }
+}
